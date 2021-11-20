19bb30a470130ad2bb1468c1544d30271dfefae7
Stephen Finch <smfinch99@gmail.com>
2021-11-19 20:25:43 -0500

    ran rolemodel rails generators

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Ensure yarn installed files are up to date when compiling webpack assets
+  config.webpacker.check_yarn_integrity = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
